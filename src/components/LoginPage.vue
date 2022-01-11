17308fda7208261dcc8e5592daef4bddbf0edde0
JosephRidge <josephridge18@gmail.com>
2022-01-11 10:24:25 +0300

    register  user  complete

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,56 +1,168 @@
 <template >
-  <div class="bg-[url('src/assets/images/background-login.svg')] bg-cover h-screen w-screen flex items-center">
- 
-<div class="container rounded bg-primaryYellow rounded-2xl w-96
-border-4 drop-shadow-2xl border-white py-2 px-4 
-w-min h-min flex flex-col  items-center
-mx-auto
- "> 
-  <div class="m-2 text-center text-white font-bold text-xl"> LOGIN </div>
-  <div class="mx-2 my-3"><input v-model="userEmail" type="email" placeholder="myemail@email.com" class="text-textgray rounded-2xl h-12 w-80 text-center" ></div>
-  <div class="mx-2 my-3"><input  v-model="userPassword" type="password" placeholder="*******"  class="rounded-2xl h-12 w-80 text-center text-textgray "></div>
-  <button @click="register" class="m-2 bg-white py-2 drop-shadow-xl transition 
-  ease-out hover:ease-in hover:drop-shadow-2xl scale-75
-   hover:scale-100 duration-300 justfy px-8 rounded-3xl 
-   font-bold w-40 mx-24 text-darkgray"> {{ actionPriorValidation }}</button>
-  <button class="m-2  transition ease-out scale-75 hover:scale-90 text-xs text-center underline text-darkgray" 
-v-on:click='registerNewUser' 
-  > No Account ? Sign Up</button>
-</div>
-   </div>
+  <div
+    class="
+      bg-[url('src/assets/images/background-login.svg')] bg-cover
+      h-screen
+      w-screen
+      flex
+      items-center
+    "
+  >
+    <div
+      class="
+        container
+        rounded
+        bg-primaryYellow
+        rounded-2xl
+        w-96
+        border-4
+        drop-shadow-2xl
+        border-white
+        py-2
+        px-4
+        w-min
+        h-min
+        flex flex-col
+        items-center
+        mx-auto
+      "
+    >
+      <div class="m-2 text-center text-white font-bold text-xl">LOGIN</div>
+      <div class="mx-2 my-3">
+        <input
+          v-model="userEmail"
+          type="email"
+          placeholder="myemail@email.com"
+          class="text-textgray rounded-2xl h-12 w-80 text-center"
+        />
+      </div>
+      <div class="mx-2 my-3">
+        <input
+          v-model="userPassword"
+          type="password"
+          placeholder="*******"
+          class="rounded-2xl h-12 w-80 text-center text-textgray"
+        />
+      </div>
+      <button
+        @click="checkUserRequest"
+        class="
+          m-2
+          bg-white
+          py-2
+          drop-shadow-xl
+          transition
+          ease-out
+          hover:ease-in hover:drop-shadow-2xl
+          scale-75
+          hover:scale-100
+          duration-300
+          justfy
+          px-8
+          rounded-3xl
+          font-bold
+          w-40
+          mx-24
+          text-darkgray
+        "
+      >
+        {{ actionPriorValidation }}
+      </button>
+      <button
+        class="
+          m-2
+          transition
+          ease-out
+          scale-75
+          hover:scale-90
+          text-xs text-center
+          underline
+          text-darkgray
+        "
+        v-on:click="switchAction"
+      >
+        No Account ? Sign Up
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
-// import firebase from 'firebase'
-export default {
-  data(){
-    return{
-      userEmail:'',
-      userPassword:'',
-      actionPriorValidation:'Login'
-    }
-  }, 
-  mounted(){
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-  }, 
-  methods:{
-    validateUser(){
-     let email  =  this.userEmail;
-     console.log("email, ",email) 
-    this.actionProirValidation = "Login"
+export default {
+  data() {
+    return {
+      userEmail: "",
+      userPassword: "",
+      actionPriorValidation: "Login",
+      action: 0
+    };
+  },
+  mounted() {},
+  methods: {
+    validateUser(auth) {
+      let email = this.userEmail;
+      console.log("email, ", email);
+      this.actionProirValidation = "Login";
+      this.action = 0 
     },
-   registerNewUser(){
-   this.actionPriorValidation = "Register" 
-   console.log("clicked", this.userPassword) 
-   console.log("clicked", this.userEmail) 
-   } 
-  }
-  
-}
+    registerNewUser(auth) {
+      console.log("clicked", this.userPassword);
+      console.log("clicked", this.userEmail);
+      let email = this.userEmail;
+      let password = this.userPassword;
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(
+            "errorr === > ",
+            errorMessage,
+            "error message --- > ",
+            errorCode
+          );
+        });
+    },
+    switchAction(){
+      this.actionPriorValidation = "Register"
+      this.action = 1
+    },
+    checkUserRequest() {
+      const firebaseConfig = {
+        apiKey: "AIzaSyDguNf-sooubRJbfMJPsKSE6LTa7mQwMwM",
+        authDomain: "umoja-assist.firebaseapp.com",
+        projectId: "umoja-assist",
+        storageBucket: "umoja-assist.appspot.com",
+        messagingSenderId: "716904160676",
+        appId: "1:716904160676:web:bba0bb7cf2919c3d3e3531",
+        measurementId: "G-9QKKGDBNS9",
+      };
+
+       initializeApp(firebaseConfig);
+      // initializeApp(Secrets.firebaseConfig);
+      console.log(" current actiom" , this.actionPriorValidation,  "action ",this.action)
+      const auth = getAuth(); 
+      console.log(typeof(this.actionProirValidation)) 
+      if (this.action === 0) {
+        this.validateUser(auth);
+      }
+      if (this.action === 1) {
+        this.registerNewUser(auth);
+      } else {
+        console.log(" Kindly confirm request once more . . . . .");
+      }
+    },
+  },
+};
 </script>
 
 <style>
-svg{ 
-  overflow-x : hidden; 
+svg {
+  overflow-x: hidden;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style/index.css'
-import { initializeApp } from 'firebase/app'
-// import firebase from 'firebase'
+// import { initializeApp } from 'firebase/app'
+// import Secrets from './secrets/secrets.js'  
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDguNf-sooubRJbfMJPsKSE6LTa7mQwMwM",
-    authDomain: "umoja-assist.firebaseapp.com",
-    projectId: "umoja-assist",
-    storageBucket: "umoja-assist.appspot.com",
-    messagingSenderId: "716904160676",
-    appId: "1:716904160676:web:bba0bb7cf2919c3d3e3531",
-    measurementId: "G-9QKKGDBNS9"
-  };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDguNf-sooubRJbfMJPsKSE6LTa7mQwMwM",
+//   authDomain: "umoja-assist.firebaseapp.com",
+//   projectId: "umoja-assist",
+//   storageBucket: "umoja-assist.appspot.com",
+//   messagingSenderId: "716904160676",
+//   appId: "1:716904160676:web:bba0bb7cf2919c3d3e3531",
+//   measurementId: "G-9QKKGDBNS9"
+// };
 
-  initializeApp(firebaseConfig);
-//   const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig)
+// // initializeApp(Secrets.firebaseConfig);
+ 
 createApp(App).mount('#app')
