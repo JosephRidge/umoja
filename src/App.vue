5dc1bd5820165b6b0157bf192d28088c16429c05
JosephRidge <josephridge18@gmail.com>
2022-01-12 11:42:31 +0300

    Cleaner Code

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import LoginPage from './components/LoginPage.vue'
-import HomePage  from './components/HomePage.vue';
+<script setup> 
 </script>
 
 <template>
